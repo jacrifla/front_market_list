97481ae8097805a1c2855ffd79b8fe11d0d68e74
jacrifla <jaquelini_cris@hotmail.com>
2025-06-03 21:48:54 -0300

    pagina de cadastros

--- src/pages/Registers.jsx
+++ src/pages/Registers.jsx
@@ -1,3 +1,38 @@
+import BrandRegisterForm from '../components/registers/BrandRegisterForm';
+import CategoryRegisterForm from '../components/registers/CategoryRegisterForm';
+import ItemRegisterForm from '../components/registers/ItemRegisterForm';
+import MarketRegisterForm from '../components/registers/MarketRegisterForm';
+import UnitRegisterForm from '../components/registers/UnitRegisterForm';
+
 export default function Registers() {
-  return <h1>Página Cadastro</h1>;
+  return (
+    <div className="container mt-4">
+      <h2>Cadastro</h2>
+
+      <div className="row mb-4">
+        <div className="col-md-8 d-flex">
+          <div className="w-100 h-100">
+            <ItemRegisterForm />
+          </div>
+        </div>
+        <div className="col-md-4 d-flex">
+          <div className="w-100 h-100">
+            <CategoryRegisterForm />
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4">
+          <BrandRegisterForm />
+        </div>
+        <div className="col-md-4">
+          <UnitRegisterForm />
+        </div>
+        <div className="col-md-4">
+          <MarketRegisterForm />
+        </div>
+      </div>
+    </div>
+  );
 }
